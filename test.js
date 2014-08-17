f833d4aaa0c7393e2af8c8924dbfc19eff3a3414
Guo Yu <i@guoyu.me>
2014-08-17 22:17:05 +0800

    0.1: release a stable version

--- test.js
+++ test.js
@@ -1,23 +1,27 @@
-var version = require('./index'),
-    should = require('should');
+var version = require('./index');
+var should = require('should');
 
 describe('makeVersionString', function() {
-    it('should return a version string', function() {
-        var v1 = new version(),
-            v2 = new version({raw: 100}),
-            v3 = new version({divider: '-'});
-        v1.version.should.equal('0.0.0');
-        v2.version.should.equal('1.0.0');
-        v3.version.should.equal('0-0-0');
+  it('should return a version string', function() {
+    var v1 = new version();
+    var v2 = new version({
+      raw: 100
     });
+    var v3 = new version({
+      divider: '-'
+    });
+    v1.version.should.equal('0.0.0');
+    v2.version.should.equal('1.0.0');
+    v3.version.should.equal('0-0-0');
+  });
 });
 
 describe('updateVersion', function() {
-    it('should return a updated version string', function() {
-        var v1 = new version();
-        v1.up();
-        v1.version.should.equal('0.0.1');
-        v1.up();
-        v1.version.should.equal('0.0.2');
-    });
+  it('should return a updated version string', function() {
+    var v1 = new version();
+    v1.up();
+    v1.version.should.equal('0.0.1');
+    v1.up();
+    v1.version.should.equal('0.0.2');
+  });
 });
